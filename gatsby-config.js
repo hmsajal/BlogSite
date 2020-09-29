c144f359623993b63b70a1bd665086c594fd1a7e
hmsajal <19496424+hmsajal@users.noreply.github.com>
2020-09-30 02:17:29 +0600

    deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/black-pen.png`,
       },
     },
     `gatsby-plugin-sass`,
