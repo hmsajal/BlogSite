ac4a254a8d674d693a8e98d501135f2a2c9aeef8
hmsajal <19496424+hmsajal@users.noreply.github.com>
2020-10-01 00:59:07 +0600

    let's see

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,7 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
